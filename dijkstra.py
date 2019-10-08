10da7d640696000c640c6d64de4ef0e69947ade8
RomainCtl <romain.chantrel35@gmail.com>
2019-10-08 14:28:32 +0200

    __str__ display & test file

--- dijkstra.py
+++ dijkstra.py
@@ -9,19 +9,20 @@ INF = 999999999
 
 class Dijkstra:
     M: np.array
-    peak: int
+    node: int
     dist: list
     pred: list
 
-    def __init__(self, M, peak):
-        self.M = M
-        self.peak = peak
+    def __init__(self, M, node):
+        M[M == None] = INF
+        self.M = M.astype(int)
+        self.node = node
         self.dist = list()
         self.pred = list()
 
     def __construct_path(self) -> list:
         """
-        create full path of predecessor for each peak with 'self.pred' list
+        create full path of predecessor for each node with 'self.pred' list
         """
         res = []
         for i in range(len(self.pred)):
@@ -33,58 +34,58 @@ class Dijkstra:
                 res[i] += (j,)
                 j = self.pred[j]
 
-        # add parent peak
+        # add parent node
         for i in range(len(self.pred)):
             if self.dist[i] != INF:
-                res[i] += (peak,)
+                res[i] += (self.node,)
                 res[i] = res[i][::-1]  # reverse
                 res[i] += (i,)
         return res
 
     def calc(self) -> None:
         """
-        this function calc distance from selected peak each others peak
-        and predecessor of the smallest path of each peak
+        this function calc distance from selected node each others node
+        and predecessor of the smallest path of each node
         with dijkstra algo
         """
-        if self.peak >= len(self.M):
-            raise ValueError(f"Unknown peak ! '{self.peak}'")
-        marked_peak = [self.peak]
-        others_peak = list(range(len(self.M)))
-        others_peak.remove(self.peak)
+        if self.node >= len(self.M):
+            raise ValueError(f"Unknown node ! '{self.node}'")
+        marked_node = [self.node]
+        others_node = list(range(len(self.M)))
+        others_node.remove(self.node)
 
         self.dist = list(INF for _ in range(len(self.M)))
         self.pred = list(INF for _ in range(len(self.M)))
 
         for i in range(len(self.M)):
-            self.dist[i] = self.M[self.peak, i]
-            if self.M[self.peak, i] != INF:
-                self.pred[i] = self.peak
+            self.dist[i] = self.M[self.node, i]
+            if self.M[self.node, i] != INF:
+                self.pred[i] = self.node
             else:
                 self.pred[i] = -1
 
-        self.dist[self.peak] = 0
+        self.dist[self.node] = 0
         _dist = self.dist[:]  # copy of dist
-        self.pred[self.peak] = -1
+        self.pred[self.node] = -1
 
-        while len(others_peak) != 0:
+        while len(others_node) != 0:
             # choose min path
             tmp = []
             for i in range(len(self.M)):
-                if i in marked_peak:
+                if i in marked_node:
                     tmp += [INF]
                 else:
                     tmp += [self.dist[i]]
             min_r_dist = _dist.index(sorted(tmp)[0])
             _dist[min_r_dist] = INF
 
-            marked_peak += [min_r_dist]
-            if min_r_dist not in others_peak:
+            marked_node += [min_r_dist]
+            if min_r_dist not in others_node:
                 # no more path
                 break
-            others_peak.remove(min_r_dist)
+            others_node.remove(min_r_dist)
 
-            for p in others_peak:
+            for p in others_node:
                 if self.dist[min_r_dist] + self.M[min_r_dist, p] < self.dist[p]:
                     _dist[p] = self.dist[p] = (
                         self.dist[min_r_dist] + self.M[min_r_dist, p]
@@ -100,7 +101,7 @@ class Dijkstra:
         cells = []
 
         for i in range(len(self.dist)):
-            if i != self.peak:
+            if i != self.node:
                 p = "->".join(map(str, self.pred[i])) or "<aucun>"
                 d = (
                     "{:^17d}".format(self.dist[i])
@@ -109,11 +110,11 @@ class Dijkstra:
                 )
                 cells.append(["{:^6d}".format(i), d, p])
 
-        N = self.M[:]
-        N[N == INF] = 0  # replace 'INF' by '0'
-        G = nx.from_numpy_array(N, create_using=nx.DiGraph())
+        self.M[self.M == INF] = 0  # replace 'INF' by '0'
+        G = nx.from_numpy_array(self.M, create_using=nx.DiGraph())
         pos = nx.circular_layout(G)
         edge_label = nx.get_edge_attributes(G, "weight")
+
 
         fig, (a0, a1) = plt.subplots(2, 1, gridspec_kw={"height_ratios": [1, 1.5]})
 
@@ -123,20 +124,38 @@ class Dijkstra:
             cellText=cells,
             colLabels=[
                 "Sommet",
-                f"Distance depuis {self.peak}",
+                f"Distance depuis {self.node}",
                 "Chemin le plus court",
             ],
             loc="center",
         )
-        a0.set_title(f"Sommet de départ : {self.peak}")
+        a0.set_title(f"Sommet de départ : {self.node}")
 
         # directed graph
         warnings.simplefilter("ignore")
         nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_label, ax=a1)
-        nx.draw_networkx(G, pos, node_size=500, with_labels=True, arrows=True, ax=a1)
+        nx.draw_networkx(G, pos, node_size=500, with_labels=True, arrows=True, connectionstyle='arc3,rad=0.06', ax=a1)
+        a1.set_title("*lorsqu'une liaison entre deux sommets est à double sens,\n il n'y a que un coût de transfer affiché (sur 2), veuillez vérifier avec la matrice affiché dans la console.", fontdict=dict(fontsize=8))
 
         fig.tight_layout()
         plt.show()
+
+    def __str__(self) -> str:
+        """
+        return object as string
+        """
+        res = f"Sommet de départ : {self.node}\n"
+        res += f"Sommet | Distance depuis {self.node} | Chemin le plus court\n"
+        for i in range(len(self.dist)):
+            if i != self.node:
+                p = "->".join(map(str, self.pred[i])) or "<aucun>"
+                d = (
+                    "{:^17d}".format(self.dist[i])
+                    if self.dist[i] != INF
+                    else "    <infini>     "
+                )
+                res += "{:^6d} | {} | {}\n".format(i, d, p)
+        return res
 
 
 if __name__ == "__main__":
@@ -151,8 +170,9 @@ if __name__ == "__main__":
             [INF, INF, INF, 1, INF, INF, INF],
         ]
     )
-    peak = 0
+    node = 0
 
-    obj = Dijkstra(M, peak)
+    obj = Dijkstra(M, node)
     obj.calc()
+    print(obj)
     obj.draw()
---
+++ dijkstra_test.py
@@ -0,0 +1,133 @@
+import argparse, sys
+import numpy as np
+from random import randint
+
+from dijkstra import Dijkstra
+
+MATRICES = [
+    np.array(
+        [
+            [None, 2, 3, None, None, None, None],
+            [None, None, None, None, None, 10, None],
+            [None, 3, None, None, None, None, None],
+            [None, None, None, None, None, None, None],
+            [None, None, 2, 5, None, None, 1],
+            [None, None, 4, 7, 3, None, 5],
+            [None, None, None, 1, None, None, None],
+        ]
+    ),
+    np.array(
+        [
+            [None, 1, 3, None],
+            [None, 2, None, None],
+            [None, 1, None, 4],
+            [None, None, 4, None],
+        ]
+    ),
+    np.array(
+        [
+            [None, 2, 6, None, None, 10],
+            [None, None, 4, None, 3, None],
+            [None, None, None, 6, None, None],
+            [None, None, None, None, 1, None],
+            [None, None, 5, None, None, None],
+            [None, 3, None, None, 1, None],
+        ]
+    ),
+    np.array([[None, None, None], [1, None, 2], [3, None, None]]),
+    np.array(
+        [
+            [None, 1, None, 3, 4, None, None, 9],
+            [None, None, None, None, 1, None, 3, 4],
+            [2, None, None, 5, None, None, 6, None],
+            [None, None, None, None, 1, None, None, None],
+            [None, None, None, None, None, None, 4, 2],
+            [None, None, 7, None, None, None, None, None],
+            [6, None, None, None, None, 6, None, 8],
+            [None, None, None, 9, None, None, None, None],
+        ]
+    ),
+]
+
+
+class CustomError(Exception):
+    pass
+
+
+class Dijkstra_test:
+    def __init__(self, mat, node, draw, random):
+        if not random:
+            if mat not in range(len(MATRICES)):
+                raise CustomError(
+                    f"Matrice inexistante ! (matrice disponibles: {list(range(len(MATRICES)))})"
+                )
+
+            M = MATRICES[mat]
+
+            if node not in range(len(M)):
+                raise CustomError(
+                    f"Sommet {node} inexistant ! (noeuds disponibles: {list(range(len(M)))} pour la matrice {mat})"
+                )
+        else:
+            M = self.generate_random()
+
+        print(f"Matrice {mat if not random else 'aléatoire'} (None = pas de chemin): \n", M, "\n")
+
+        obj = Dijkstra(M, node)
+        obj.calc()
+
+        print(obj)  # afficher le résulat dans la console
+
+        if draw:
+            obj.draw()  # afficher le graphe et le résulat sur une interface graphique
+
+    def generate_random(self, max_int=10):
+        s = randint(2, 8)
+        attr = [None] + list(range(1,max_int))
+        max_int -= 1
+        perc = [0.6] + [0.4/max_int] * max_int
+        return np.random.choice(attr, size=(s,s), p=perc)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-m",
+        "--matrice",
+        type=int,
+        default=0,
+        help="Un entier identifiant de la matrice à utiliser",
+    )
+    parser.add_argument(
+        "-n",
+        "--node",
+        type=int,
+        default=0,
+        help="Sommet de départ de la matrice choisie",
+    )
+    parser.add_argument(
+        "-d",
+        "--draw",
+        dest="draw",
+        action="store_true",
+        help="Afficher l'interface graphique (graphe et résultat de l'algorithle de Dijkstra)",
+    )
+    parser.add_argument(
+        "-r",
+        "--random",
+        dest="random",
+        action="store_true",
+        help="Générer une matrice aléatoire à utiliser",
+    )
+    args = parser.parse_args()
+
+    try:
+        Dijkstra_test(args.matrice, args.node, args.draw, args.random)  # lancer le test
+    except CustomError as e:
+        print("Error :\n", e, "\n")
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
